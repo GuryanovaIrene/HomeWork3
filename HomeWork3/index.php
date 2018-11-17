724869a7a3b130d508e0d82a70d9bd5cbccce98c
GuryanovaIrene <44869308+GuryanovaIrene@users.noreply.github.com>
2018-11-17 18:32:40 +0300

    Add files via upload

--- HomeWork3/index.php
+++ HomeWork3/index.php
@@ -1,4 +1,59 @@
 <pre>
 <?php
 require "src/functions.php";
-task1('data.xml');
+//task1('data.xml');
+
+$arr = array(
+    'Keyboard' => array(
+        'Defender OfficeMate SM-820' => array(
+            'Connector' => 'USB',
+            'Keyboard Type' => 'Membrane',
+            'Key illumination' => 'No',
+            'Silent keys' => 'Yes',
+            'Digital block' => 'Yes',
+            'Connection type' => 'Wired'
+        ),
+        'DEXP KW-3002BU' => array(
+            'Connector' => 'USB',
+            'Keyboard Type' => 'Membrane',
+            'Key illumination' => 'No',
+            'Silent keys' => 'Yes',
+            'Digital block' => 'Yes',
+            'Connection type' => 'Not wired'
+        )
+    ),
+    'Mouse' => array(
+        'DEXP CM-407BU black' => array(
+            'Main color' => 'Black',
+            'Illumination' => 'No',
+            'Total number of buttons' => 3,
+            'Additional buttons' => 'No',
+            'Sensor type' => 'Optical led sensor',
+            'Max resolution of the sensor (dpi)' => '800 dpi',
+            'Connection type' => 'Wired'
+        ),
+        'DEXP WM-903BU black' => array(
+            'Main color' => 'Black',
+            'Illumination' => 'No',
+            'Total number of buttons' => 6,
+            'Additional buttons' => 'Yes',
+            'Sensor type' => 'Optical led sensor',
+            'Max resolution of the sensor (dpi)' => '1600 dpi',
+            'Connection type' => 'Not wired'
+        ),
+        'DEXP WM-903BU red' => array(
+            'Main color' => 'Red',
+            'Illumination' => 'No',
+            'Total number of buttons' => 3,
+            'Additional buttons' => 'No',
+            'Sensor type' => 'Optical led sensor',
+            'Max resolution of the sensor (dpi)' => '800 dpi',
+            'Connection type' => 'Not wired'
+        )
+    )
+);
+task2($arr);
+
+echo task3(60) . PHP_EOL;
+
+echo task4('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
--- HomeWork3/src/functions.php
+++ HomeWork3/src/functions.php
@@ -51,3 +51,77 @@ function task1($fileName) {
     }
 }
 
+function task2($arr) {
+    file_put_contents('output.json', json_encode($arr));
+
+    $js = file_get_contents('output.json');
+    $arr2 = json_decode($js, true);
+
+    if (rand(0, 1)) {
+        $arr2['Keyboard']['Oklick 920G'] = array(
+            'Connector' => 'USB',
+            'Keyboard Type' => 'Mechanical',
+            'Key illumination' => 'Yes',
+            'Silent keys' => 'No',
+            'Digital block' => 'Yes',
+            'Connection type' => 'Wired'
+        );
+
+        $arr2['Mouse']['Type']['DEXP WM-903BU black']['Illumination'] = 'Yes';
+
+        $arr2['Monitor'] = 'LG';
+    }
+    file_put_contents('output2.json', json_encode($arr2));
+
+    $js = file_get_contents('output.json');
+    $js2 = file_get_contents('output2.json');
+
+    $arrGet = json_decode($js, true);
+    $arrGet2 = json_decode($js2, true);
+
+
+}
+
+function task3($n) {
+    if ($n >= 50) {
+        $arr = array();
+        for ($i = 1; $i <= $n; $i++) {
+            array_push($arr, rand(1, 100));
+        }
+        if ($fp = fopen('numbers.csv', 'w')) {
+            fputcsv($fp, $arr, ';');
+            fclose($fp);
+            $sum = 0;
+            if ($fp = fopen('numbers.csv', 'r')) {
+                while ($csvData = fgetcsv($fp, 100, ';')) {
+                    foreach ($csvData as $num) {
+                        $num = number_format($num);
+                        if ($num % 2 == 0) {
+                            $sum += $num;
+                        }
+                    }
+                }
+                return $sum;
+            } else {
+                return 'Невозможно считать csv-файл!';
+            }
+        } else {
+            return 'Невозможно записать в csv-файл!';
+        }
+    } else {
+        return 'Введите число не менее 50!!!';
+    }
+}
+
+function task4($url) {
+    $result = file_get_contents($url);
+    $title = strstr($result, '"title":');
+    $title = substr($title,1, strpos($title, ',') - 2);
+    $title = str_replace('":"', '=', $title);
+
+    $pageId = strstr($result, '"pageid":');
+    $pageId = substr($pageId,1, strpos($pageId, ',') - 2);
+    $pageId = str_replace('":"', '=', $pageId);
+
+    return $title . PHP_EOL . $pageId;
+}
